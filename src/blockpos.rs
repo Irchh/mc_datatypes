2b28eb6c1e58b94b39aac564b5b0379f654e0002
Irjan Vuckovic Olsen <irjan.olsen@gmail.com>
2024-06-30 19:50:28 +0200

    Added functionality to the BlockPos struct

--- src/blockpos.rs
+++ src/blockpos.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialOrd, PartialEq)]
 pub struct BlockPos {
     x: i32,
     y: i32,
@@ -12,4 +12,12 @@ impl BlockPos {
     pub fn packed(&self) -> u64 {
         (self.x as u64&0x3FFFFFF) << 38 | (self.z as u64&0x3FFFFFF) << 12 | (self.y as u64&0xFFF)
     }
+
+    pub fn x(&self) -> i32 { self.x }
+    pub fn y(&self) -> i32 { self.y }
+    pub fn z(&self) -> i32 { self.z }
+
+    pub fn set_x(&mut self, value: i32) { self.x = value; }
+    pub fn set_y(&mut self, value: i32) { self.y = value; }
+    pub fn set_z(&mut self, value: i32) { self.z = value; }
 }
